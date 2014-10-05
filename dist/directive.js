7a6d59bbf00c4d562f7024880fd52b5496b53dde
Mohsen Azimi <mazimi@apigee.com>
2014-10-05 02:13:14 -0700

    Gulpfile stuff

---
+++ dist/directive.js
@@ -0,0 +1,28 @@
+/*!
+ * angular-directive-template
+ * 
+ * Version: 0.0.0 - 2014-10-05T09:12:19.756Z
+ * License: MIT
+ */
+
+
+angular.module("the.directive").run(["$templateCache", function($templateCache) {$templateCache.put("directive.html","<div class=\"the-directive\"><div>value is {{value}}</div><button ng-click=\"increment()\"></button></div>");}]);
+angular.module('the.directive', []).directive('the.directive', TheDirective);
+
+function TheDirective() {
+  return {
+    restrict: 'AE',
+    template: 'src/directive.html',
+    replcae: true,
+    link: function ($scope) {
+      var value = 0;
+
+      $scope.getValue = function () {
+        return value;
+      };
+      $scope.increment = function () {
+        value++;
+      };
+    }
+  };
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,32 @@
 var fs = require('fs');
-var gulp = require('gulp');
 var connect = require('gulp-connect');
+var gulp = require('gulp');
+var karma = require('karma').server;
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var header = require('gulp-header');
+var rename = require('gulp-rename');
+var es = require('event-stream');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCSS = require('gulp-minify-css');
+var templateCache = require('gulp-angular-templatecache');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var open = require('gulp-open');
+
+
+var config = {
+  pkg : JSON.parse(fs.readFileSync('./package.json')),
+  banner:
+      '/*!\n' +
+      ' * <%= pkg.name %>\n' +
+      ' * <%= pkg.homepage %>\n' +
+      ' * Version: <%= pkg.version %> - <%= timestamp %>\n' +
+      ' * License: <%= pkg.license %>\n' +
+      ' */\n\n\n'
+};
 
 gulp.task('connect', function() {
   connect.server({
@@ -18,4 +44,55 @@ gulp.task('watch', function () {
   gulp.watch(['./demo/*.*'], ['html']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('clean', function(cb) {
+  del(['dist'], cb);
+});
+
+gulp.task('scripts', ['clean'], function() {
+
+  function buildTemplates() {
+    return gulp.src('src/**/*.html')
+      .pipe(minifyHtml({
+             empty: true,
+             spare: true,
+             quotes: true
+            }))
+      .pipe(templateCache({module: 'the.directive'}));
+  };
+
+  function buildDistJS(){
+    return gulp.src('src/directive.js')
+      .pipe(plumber({
+        errorHandler: handleError
+      }))
+      .pipe(jshint())
+      .pipe(jshint.reporter('jshint-stylish'))
+      .pipe(jshint.reporter('fail'));
+  };
+
+  es.merge(buildDistJS(), buildTemplates())
+    .pipe(plumber({
+      errorHandler: handleError
+    }))
+    .pipe(concat('directive.js'))
+    .pipe(header(config.banner, {
+      timestamp: (new Date()).toISOString(), pkg: config.pkg
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('directive.min.js'))
+    .pipe(uglify({preserveComments: 'some'}))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('open', function(){
+  gulp.src('./demo/demo.html')
+  .pipe(open('', {url: 'http://localhost:8080/demo/demo.html'}));
+});
+
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+};
+
+gulp.task('serve', ['connect', 'watch', 'open']);
+gulp.task('default', ['scripts']);
